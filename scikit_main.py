36430af9ad8b62b0e6c3834f5645a4c18912dcc2
lmwis <detectiveygn@163.com>
2021-11-30 13:20:05 +0800

    scikit-lear linner regression test

--- scikit_main.py
+++ scikit_main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 
@@ -9,27 +9,29 @@ def main():
     data = pd.read_csv('housing_price_init.csv')
     print(data.shape)
     y = data['price']
-    x = data['area']
+    x = data.drop('price', axis=1)
 
-    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
     print(x_train.shape)
     print(x_test.shape)
     print(y_train.shape)
+    # x_train = x_train.reshape(-1,1)
+    lm = linear_model.LinearRegression()
+    lm.fit(X=x_train, y=y_train)
 
-    lm = linear_model.LinearRegression
-    lm.fit(self=lm,X=x_train,y=y_train)
+    print("截距：", lm.coef_)
+    print("斜率：", lm.intercept_)
 
-    print("截距：",lm.coef_)
-    print("斜率：")
+    draw_result(lm.intercept_, lm.coef_, data)
 
-    draw_result(lm.intercept_,lm.coef_,data)
 
-def draw_result(o0,o1,data):
-    x = np.linspace(20,2000,40)
-    y = o0+x*o1
-    plt.plot(x,y,color='red')
+def draw_result(o0, o1, data):
+    x = np.linspace(20, 2000, 40)
+    y = o0 + x * o1
+    plt.plot(x, y, color='red')
     draw_dot(data)
+
 
 def draw_dot(data):
     # draw
@@ -38,6 +40,6 @@ def draw_dot(data):
     plt.ylabel('price')
     plt.show()
 
+
 if __name__ == '__main__':
     main()
-

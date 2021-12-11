2f243671dd8ce107120c59aa9cf94b375a4acca4
lmwis <detectiveygn@163.com>
2021-12-11 20:35:02 +0800

    one hot code and theta value

--- logistic_regression/scikit_logistic_regression_main.py
+++ logistic_regression/scikit_logistic_regression_main.py
@@ -4,52 +4,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score, train_test_split
+import sklearn.preprocessing as preprocessing
 
 
 def main():
+    pd.set_option('display.width', 130)
+    pd.set_option('display.max_columns', 130)
+    pd.set_option('display.max_colwidth', 130)
     data = pd.read_csv('train.csv')
-    print(data.shape)
-    print(data['SibSp'].head())
-    print(data.columns)
+    # print(data.shape)
+    # print(data['SibSp'].head())
+    # print(data.columns)
 
 
     # test_data = pd.DataFrame(data,columns=['Survived','Pclass','Sex'])
     # draw_dot(test_data)
     used_feature = ['Pclass', 'Sex', 'Age','Embarked','SibSp','Parch']
-
-
-
-
+    used_feature_regex = 'Age_.*|Pclass_.*|Cabin_.*|Embarked_.*|SibSp|Parch|Fare_.*|Sex_.*'
     # print(data['Cabin'].info())
-    # data = data_clean(data)
-    print(data.isnull().sum())
+    data = data_clean(data)
+
+    lr = build_module_regex(data,used_feature_regex)
+
+    used_feature = pd.DataFrame(data).filter(regex=used_feature_regex).columns.values
     # print(data[data['Cabin'].notnull()].value_counts())
     # data = set_cabin_type(data)
     # print(data['Cabin'].head())
     # print(data['Sex'].head())
-    # build_module(data,used_feature)
+    # lr = build_module(data,used_feature)
 
-    # cabin_data = pd.DataFrame(data, columns=['Survived', 'Cabin'])
-    # print(cabin_data.head())
-    # print(cabin_data.describe())
-    #tsasw                                                                                                              /?
-    # embarked_data = pd.DataFrame(data, columns=['Survived', 'Embarked'])
-    # print(embarked_data.head())
-    # print(embarked_data.describe())
-    #
-    # print(embarked_data.columns)
-    # embarked_data = data_clean(embarked_data)
+    theta = pd.DataFrame({"colums":used_feature,"coef":list(lr.coef_.T)})
+    print(theta)
 
-
-    #
+    # view data with graph
     # analyze_feature(data)
 
+# scaling feature to [-1,1]
+def scaling_data(data,feature):
+    scaler = preprocessing.StandardScaler()
+    for i in feature:
+        param = scaler.fit(data[i].values.reshape(-1,1))
+        data[i+'_scaled'] = scaler.fit_transform(data[i].values.reshape(-1,1),param)
+    return data
 # change 'Cabin' feature to 'Yes' or 'No'
-def set_cabin_type(data):
-    data.loc[data['Cabin'].notnull()]='Yes'
-    data.loc[data['Cabin'].isnull()]='No'
+
+# set missing age whit random forest
+def set_missing_ages(data):
+    age_data = pd.DataFrame(data,columns=['Age','Fare','Parch','SibSp','Pclass'])
+    known_age = age_data[age_data['Age'].notnull()].values
+    unknown_age = age_data[age_data['Age'].isnull()].values
+
+    y = known_age[:,0] # 年龄列
+    X = known_age[:,1:] # 其他特征列
+    # print(age_data.value_counts())
+    # 随机森林进行拟合
+    rf = RandomForestRegressor(random_state=0,n_estimators=2000,n_jobs=-1)
+    rf.fit(X,y)
+    # 进行预测
+    predict_age = rf.predict(unknown_age[:,1::])
+    data.loc[data['Age'].isnull(),'Age'] = predict_age
     return data
 
+def set_cabin_type(data):
+    data.loc[data['Cabin'].notnull(),'Cabin']='Yes'
+    data.loc[data['Cabin'].isnull(),'Cabin']='No'
+    return data
+def set_one_hot_encode(data):
+    dummies_Cabin = pd.get_dummies(data['Cabin'], prefix='Cabin')
+    dummies_Embarked = pd.get_dummies(data['Embarked'], prefix='Embarked')
+    dummies_Sex = pd.get_dummies(data['Sex'], prefix='Sex')
+
+    dummies_Pclass = pd.get_dummies(data['Pclass'], prefix='Pclass')
+
+    data = pd.concat([data, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+    data.drop(['Cabin', 'Name', 'Ticket', 'Embarked', 'Sex', 'Pclass'], axis=1, inplace=True)
+    return data
 def analyze_feature(data):
     class_data = pd.DataFrame(data, columns=['Survived', 'Pclass'])
     sex_data = pd.DataFrame(data, columns=['Survived', 'Sex'])
@@ -83,12 +114,17 @@ def analyze_feature(data):
     draw_continuous_feature_to_survived_data_line(fare_data, 'Fare') # not sure
     # draw_continuous_feature_to_survived_data_line(ticket_data, 'Ticket')
 
+# use regex to pick feature
+def build_module_regex(train_data,regex):
+    used_feature = pd.DataFrame(train_data).filter(regex=regex).columns.values
+    return build_module(train_data,used_feature)
+
 def build_module(train_data,used_feature):
     data = train_data
     # pick feature 'Pclass' and 'Sex' for logistic regression
+    # use filer to pick feature
     train_X = pd.DataFrame(data, columns=used_feature)
     # train_X = data_clean(train_X)
-    print(train_X.isna)
     # checkout if has nan value
     # print(train_X[train_X['Age'].isnull().values==True].head())
     y = pd.DataFrame(data, columns=['Survived'])
@@ -96,31 +132,43 @@ def build_module(train_data,used_feature):
 
     # use module to predict test data
     test_data = pd.read_csv('test.csv')
-    test_X = pd.DataFrame(test_data, columns=used_feature)
-    test_X = data_clean(test_X)
+    test_X = data_clean(test_data)
+    test_X = pd.DataFrame(test_X, columns=used_feature)
+    # test_X = set_cabin_type(test_X)
     output_y = lr.predict(test_X)
 
     # save result
     result = pd.DataFrame(test_data, columns=['PassengerId'])
     result['Survived'] = pd.Series(output_y)
-    print(result.head())
 
     pd.DataFrame.to_csv(result, path_or_buf='result.csv', index=False)
     print('输出结果完成')
 
+    return lr
+
 def data_clean(data):
-    if 'Sex' in data.columns:
-        data['Sex'] = data['Sex'].map({
-            'male': 1,
-            'female': 0
-        })
+
+    # if 'Sex' in data.columns:
+    #     data['Sex'] = data['Sex'].map({
+    #         'male': 1,
+    #         'female': 0
+    #     })
     # fill the missing age with mean value
-    if 'Age' in data.columns:
-        data['Age'] = data['Age'].fillna(value=data['Age'].mean())
+    # if 'Age' in data.columns:
+    #     data['Age'] = data['Age'].fillna(value=data['Age'].mean())
+
+    if 'Fare' in data.columns:
+        data['Fare'] = data['Fare'].fillna(value=data['Fare'].mean())
 
     if 'Embarked' in data.columns:
         data['Embarked'] = data['Embarked'].fillna(value=data['Embarked'].value_counts().idxmax())
-        data['Embarked'] = translate_enum(data['Embarked'])
+        # data['Embarked'] = translate_enum(data['Embarked'])
+    print(data.head())
+    data = set_cabin_type(data)
+    data = set_missing_ages(data)
+    data = set_one_hot_encode(data)
+    data = scaling_data(data,['Age','Fare'])
+
     return data
 
 def translate_enum(series):
